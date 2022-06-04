6b79f93258e1febfdf8e76e907d4235732680c2e
bayronq archlinux portatil <bayronq@gmail.com>
2022-06-04 12:31:37 -0600

    Se actualizó script de instalación

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #oh my zshrc
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #zsh suggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
@@ -10,6 +10,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 #powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+cp ~/Repo/mydotfiles/.p10k.zsh ~/.p10k.zsh
 
 #Buscador fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -18,4 +19,17 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 rm ~/.zshrc
 
 cp ~/Repo/mydotfiles/.zshrc ~/.zshrc
-cp ~/Repo/mydotfiles/.p10k.zsh ~/.p10k.zsh
+
+#Instalación de Plugins en neovim
+
+#Descargar repo
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#copiar carpeta nvim al pat:
+cp ~/Repo/mydotfiles/.config/nvim ~/.config/nvim
+
+#Instalar Plugins de neovim
+nvim +PlugIntall
+
+#reiniciar archivo init.vim
+#source ~/.confing/nvim/init.vim
