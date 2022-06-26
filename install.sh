0857d2c4f6d23688f1dcf2b11751682da10ecae1
bayronq archlinux portatil <bayronq@gmail.com>
2022-06-25 23:48:14 -0600

    Actualización nvim

--- install.sh
+++ install.sh
@@ -17,19 +17,17 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 rm ~/.zshrc
-
 cp ~/Repo/mydotfiles/.zshrc ~/.zshrc
 
 #Instalación de Plugins en neovim
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 ; nvim
 
 #Descargar repo
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 #copiar carpeta nvim al pat:
-cp ~/Repo/mydotfiles/.config/nvim ~/.config/nvim
+#cp ~/Repo/mydotfiles/.config/nvim ~/.config/nvim
 
-#Instalar Plugins de neovim
-nvim +PlugIntall
 
 #reiniciar archivo init.vim
 #source ~/.confing/nvim/init.vim
