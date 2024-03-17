b763befbefb74b0e74c1cf0f3a238793fa1e60a0
Bayron in Zenbook <bayronq@gmail.com>
2024-03-16 20:34:20 -0600

    separando configuraciones de nvim

--- install.sh
+++ install.sh
@@ -23,15 +23,3 @@ curl -sS https://starship.rs/install.sh | sh
 mv ~/.zshrc ~/.zshrc.bk
 cp ~/Repo/mydotfiles/.zshrc ~/.zshrc
 
-#LazyVim
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-
-#Instalación de Plugins en neovim
-#git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 ; nvim
-#Descargar repo
-#curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-#copiar carpeta nvim al pat:
-#cp ~/Repo/mydotfiles/.config/nvim ~/.config/nvim
-
-#reiniciar archivo init.vim
-#source ~/.confing/nvim/init.vim
